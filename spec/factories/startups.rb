9b73ae995769ee18c07b95744fb04b163dba0456
Luiz Carvalho <maximusmano@gmail.com>
2016-04-07 14:16:19 -0300

    Add tests for presence of name and description on Startup

--- spec/factories/startups.rb
+++ spec/factories/startups.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :startup do
-    name "MyString"
-    description "MyText"
-    logo "MyString"
-    category "MyString"
-    stage "MyString"
-    country "MyString"
-    state "MyString"
-    city "MyString"
-    site "MyString"
-    contact "MyString"
-    latitude "MyString"
-    longitude "MyString"
-    group "MyString"
-    fundation "2016-04-06"
+    name "VigilantesApp"
+    description "App of Vigilance"
+    logo "images/logo.png"
+    category :security
+    stage :ideacao
+    country :brazil
+    state :tocantins
+    city 'Palmas'
+    site 'vigilantesapp.com' 
+    contact 'contato@vigilantesapp.com'
+    latitude nil
+    longitude nil
+    group :movimento_statup_tocantins
+    fundation "2015-04-06"
   end
 end
--- spec/models/startup_spec.rb
+++ spec/models/startup_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Startup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:startup) { FactoryGirl.build(:startup) }
+
+  describe 'need' do
+    it "name" do
+      startup.name = nil
+      startup.save
+      expect(startup.errors.messages).to have_key(:name)      
+    end
+
+    it "description" do
+      startup.description = nil
+      startup.save
+      expect(startup.errors.messages).to have_key(:description)      
+    end
+
+
+  end
+
 end
